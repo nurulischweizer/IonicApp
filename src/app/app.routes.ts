6a2267a82cd9c8f0614934584fa7f6c00808e32d
Nurul Idashazwaney Binti Rosli <nurul_i@dk-schweizer.com>
2025-01-02 14:07:14 +0800

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () =>
-      import('./theme/theme.component').then((m) => m.ThemeComponent),
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  PreloadAllModules,
-  RouteReuseStrategy,
-  provideRouter,
-  withPreloading,
-} from '@angular/router';
-import {
-  IonicRouteStrategy,
-  provideIonicAngular,
-} from '@ionic/angular/standalone';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
-import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
-
-// Call the element loader before the bootstrapModule/bootstrapApplication call
-defineCustomElements(window);
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
