76609baf7cb016f9b0bd1205983f5432efca2e6f
Nurul Idashazwaney Binti Rosli <nurul_i@dk-schweizer.com>
2025-01-02 16:12:37 +0800

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,61 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonRippleEffect,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  ellipsisHorizontalOutline,
+  homeOutline,
+  listOutline,
+  personCircleOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardContent,
+    IonIcon,
+    IonRippleEffect,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  items: any[] = [
+    {
+      title: 'Condominium',
+      image: 'assets/building.png',
+    },
+    {
+      title: 'Landed Property',
+      image: 'assets/home.png',
+    },
+    {
+      title: 'Residential Apartment',
+      image: 'assets/apartment.jpg',
+    },
+  ];
+
+  constructor() {
+    addIcons({
+      homeOutline,
+      listOutline,
+      personCircleOutline,
+      ellipsisHorizontalOutline,
+    });
+  }
 }
